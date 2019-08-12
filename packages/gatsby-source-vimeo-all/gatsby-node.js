e6ae33c22ba53d890308143a83b18962721d6a32
mittnavnermike <mikepatterson@Mikes-MacBook-Pro-2.local>
2019-08-12 13:14:20 +0200

    :x: removed node vimeo client

--- packages/gatsby-source-vimeo-all/gatsby-node.js
+++ packages/gatsby-source-vimeo-all/gatsby-node.js
@@ -48,7 +48,22 @@ exports.sourceNodes = async ({
       headers: {
         Authorization: `bearer ${accessToken}`
       }
-    }).then(res => res.json()).then(res => resolve(res)).catch(err => reject(err));
+    }).then(res => res.json()).then(async res => {
+      let pages = Math.ceil(res.total / res.per_page);
+      let urls = new Array(pages).fill('x').map((x, i) => new Promise((resolve, reject) => {
+        fetch(`https://api.vimeo.com/me/videos?page=${i + 1}`, {
+          headers: {
+            Authorization: `bearer ${accessToken}`
+          }
+        }).then(res => res.json()).then(res => resolve(res)).catch(err => reject(err));
+      }));
+      await Promise.all([...urls]).then(values => {
+        console.log('values', values);
+        let videos = values.map(value => value.data);
+        console.log('videos', videos);
+        resolve(videos);
+      }).catch(err => reject(err));
+    }).catch(err => reject(err));
   });
   videos && videos.map(video => {
     const nodeData = {
---
+++ packages/gatsby-source-vimeo-all/src/vimeo.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch')
+
+const accessToken = '8ce7a91caa25d2af94f2e4cd4ecee30f'
+
+const videos = async () => {
+  await new Promise((resolve, reject) => {
+    fetch('https://api.vimeo.com/me/videos', {
+      headers: {
+        Authorization: `bearer ${accessToken}`
+      }
+    })
+      .then(res => res.json())
+      .then(async res => {
+        let pages = Math.ceil(res.total / res.per_page)
+        let urls = new Array(pages).fill('x').map(
+          (x, i) =>
+            new Promise((resolve, reject) => {
+              fetch(`https://api.vimeo.com/me/videos?page=${i + 1}`, {
+                headers: {
+                  Authorization: `bearer ${accessToken}`
+                }
+              })
+                .then(res => res.json())
+                .then(res => resolve(res))
+                .catch(err => reject(err))
+            })
+        )
+        await Promise.all([...urls])
+          .then(values => {
+            console.log('values', values)
+            let videos = values.map(value => value.data)
+            console.log('videos', videos)
+            resolve(videos)
+          })
+          .catch(err => reject(err))
+      })
+      .catch(err => reject(err))
+  })
+}
+
+videos()
